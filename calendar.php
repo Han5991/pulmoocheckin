8aa4fbca4a320fbd60d894d1792a1685c8c18911
rewq985 <bnbt3@naver.com>
2021-05-13 22:19:00 +0900

    [feat] 기능 추가 29일차 - 회사에서 방치당함 - 달력을 그리고 날짜를 클릭하면 해당 날짜에 출석하신 어머님들 로그 검색 가능

--- calendar.php
+++ calendar.php
@@ -1,14 +1,14 @@
-<?php 
-	// GET으로 넘겨 받은 year값이 있다면 넘겨 받은걸 year변수에 적용하고 없다면 현재 년도
-	$year = isset($_GET['year']) ? $_GET['year'] : date('Y');
-	// GET으로 넘겨 받은 month값이 있다면 넘겨 받은걸 month변수에 적용하고 없다면 현재 월
-	$month = isset($_GET['month']) ? $_GET['month'] : date('m');
+<?php
+// GET으로 넘겨 받은 year값이 있다면 넘겨 받은걸 year변수에 적용하고 없다면 현재 년도
+$year = isset($_GET['year']) ? $_GET['year'] : date('Y');
+// GET으로 넘겨 받은 month값이 있다면 넘겨 받은걸 month변수에 적용하고 없다면 현재 월
+$month = isset($_GET['month']) ? $_GET['month'] : date('m');
 
-	$date = "$year-$month-01"; // 현재 날짜
-	$time = strtotime($date); // 현재 날짜의 타임스탬프
-	$start_week = date('w', $time); // 1. 시작 요일
-	$total_day = date('t', $time); // 2. 현재 달의 총 날짜
-	$total_week = ceil(($total_day + $start_week) / 7);  // 3. 현재 달의 총 주차
+$date = "$year-$month-01"; // 현재 날짜
+$time = strtotime($date); // 현재 날짜의 타임스탬프
+$start_week = date('w', $time); // 1. 시작 요일
+$total_day = date('t', $time); // 2. 현재 달의 총 날짜
+$total_week = ceil(($total_day + $start_week) / 7);  // 3. 현재 달의 총 주차
 ?>
 
 <!DOCTYPE html>
@@ -30,19 +30,19 @@
 	<!-- 현재가 1월이라 이전 달이 작년 12월인경우 -->
 	<?php if ($month == 1): ?>
 		<!-- 작년 12월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/cal.php/?year=<?php echo $year-1 ?>&month=12">이전 달</a>
+		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year-1 ?>&month=12">이전 달</a>
 	<?php else: ?>
 		<!-- 이번 년 이전 월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/cal.php/?year=<?php echo $year ?>&month=<?php echo $month-1 ?>">이전 달</a>
+		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year ?>&month=<?php echo $month-1 ?>">이전 달</a>
 	<?php endif ?>
 
 	<!-- 현재가 12월이라 다음 달이 내년 1월인경우 -->
 	<?php if ($month == 12): ?>
 		<!-- 내년 1월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/cal.php/?year=<?php echo $year+1 ?>&month=1">다음 달</a>
+		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year+1 ?>&month=1">다음 달</a>
 	<?php else: ?>
 		<!-- 이번 년 다음 월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/cal.php/?year=<?php echo $year ?>&month=<?php echo $month+1 ?>">다음 달</a>
+		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year ?>&month=<?php echo $month+1 ?>">다음 달</a>
 	<?php endif ?>
 	<table border="1">
 		<tr> 
@@ -64,7 +64,8 @@
 						<!-- 시작 요일부터 마지막 날짜까지만 날짜를 보여주도록 -->
 						<?php if ( ($n > 1 || $k >= $start_week) && ($total_day >= $n) ): ?>
 							<!-- 현재 날짜를 보여주고 1씩 더해줌 -->
-							<?php echo $n++ ?>
+							
+							<a href="https://hounjini.cafe24.com/sangwook/read.php/?year=<?php echo $year ?>&month=<?php echo $month ?>&date=<?php echo $n ?>"><?php echo $n++ ?> </a>
 						<?php endif ?>
 					</td> 
 				<?php endfor; ?> 
--- read.php
+++ read.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=UTF-8');
 
 date_default_timezone_set('Asia/Seoul');
-$date = date("m.d", time());
+$date = $_REQUEST["year"].".".$_REQUEST["month"].".".$_REQUEST["date"];
 // 파일 열기
 $fp = fopen("save/name_table.txt", "r") or die("파일을 열 수 없습니다！");
 // 파일 내용 출력
