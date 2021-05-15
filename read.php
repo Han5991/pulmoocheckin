df9a1f32e9820f7b0a89de063493313ccb8144ae
rewq985 <bnbt3@naver.com>
2021-05-15 22:23:04 +0900

    [feat] 주말공부 야학 출첵 기능 추가 - 달력에서 날짜 검색이 안 되던 버그 수정 - 중복 체크인을 검사 기능 추가

--- read.php
+++ read.php
@@ -1,20 +1,66 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
+// 날짜 데이터 생성 및 받아오기
 date_default_timezone_set('Asia/Seoul');
-//구분할 날짜 데이터 받아오기
-$date = $_REQUEST["year"].".".$_REQUEST["month"].".".$_REQUEST["date"];
-// 파일 열기
+$year = $_REQUEST["year"];
+$month = mb_strlen($_REQUEST["month"]) == 2 ? $_REQUEST["month"] : "0" . $_REQUEST["month"];
+$day = mb_strlen($_REQUEST["day"]) == 2 ? $_REQUEST["day"] : "0" . $_REQUEST["day"];
+$date = $year . "." . $month . "." . $day;
+
+// 데이터를 읽어서 반을 구분하여 출력함
 $fp = fopen("save/name_table.txt", "r") or die("파일을 열 수 없습니다！");
-// 파일 내용 출력
+$rose = "";
+$sunflower = "";
+
 while (! feof($fp)) {
     $str = fgets($fp);
+    $strarr = explode(' ', $str);
     
-    if(strpos($str, $date) !== false) {
-        print $str;
+    $time = $strarr[1];
+    $clas = $strarr[2];
+    $name = $strarr[3];
+    $qr = $strarr[4];
+    
+    if (strpos($str, $date) !== false) {
+        
+        if (strpos($rose, $name) === false && strpos($sunflower, $name) === false) {
+            
+            if (strpos($str, "장미") !== false || strpos($str, "고급2") !== false) {
+                $rose .= "<tr><td>" . $time . " " . $clas . " " . $name . "</td></tr>";
+                
+            } else if (strpos($str, "해바라기") !== false) {
+                $sunflower .= "<tr><td>" . $time . " " . $clas . " " . $name . "</td></tr>";
+            }
+        }
     }
 }
-
-// 파일 닫기
 fclose($fp);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>calendar</title>
+</head>
+<body>
+	<?php print "$year 년 $month 월 $day 일 출석현황" ?>
+	<table>
+		<tr>
+			<th>장미반</th>
+		</tr>
+        <?php
+        mb_strlen($rose) == 0 ? print "<tr><td>아무도 안 오셨어요ㅜㅜ</td></tr>" : print $rose;
+        ?>
+	</table>
+
+	<table>
+		<tr>
+			<th>해바라기반</th>
+		</tr>
+        <?php
+        mb_strlen($sunflower) == 0 ? print "<tr><td>아무도 안 오셨어요ㅜㅜ</td></tr>" : print $sunflower;
+        ?>
+	</table>
+</body>
+</html>
