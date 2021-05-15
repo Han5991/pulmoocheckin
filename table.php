c25aa29fd3c18157164ece3c71fc87576aef9ca3
rewq985 <bnbt3@naver.com>
2021-05-15 22:53:03 +0900

    [bugfix] 주말공부 - 체크인 입력 데이터 수정

--- table.php
+++ table.php
@@ -5,14 +5,13 @@ date_default_timezone_set('Asia/Seoul');
 $clas=$_POST['clas'];
 
 $content = "";
-$content .= date("m.d\tH:i:s", time());
-$content .= "\t";
+$content .= date("Y.m.d H:i:s", time());
+$content .= ' ';
 $content .= $clas;
-$content .= "\t";
+$content .= ' ';
 $content .= $_POST['name'];
-$content .= "\t";
+$content .= ' ';
 $content .= $_POST['org'];
-$content .= "<br>";
 $content .= "\n";
 
 if((strpos($clas, "장미") !== false) || (strpos($clas, "고급2") !== false)) {
