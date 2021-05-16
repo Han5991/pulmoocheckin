646f5b9979c6c35412d1c67b803dc097d6563a7c
rewq985 <bnbt3@naver.com>
2021-05-16 11:03:07 +0900

    [feat] 주말공부 - 테이블 그리는 중 졸라 어렵네

--- calendar.php
+++ calendar.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
+
 // GET으로 넘겨 받은 year값이 있다면 넘겨 받은걸 year변수에 적용하고 없다면 현재 년도
 $year = isset($_GET['year']) ? $_GET['year'] : date('Y');
 // GET으로 넘겨 받은 month값이 있다면 넘겨 받은걸 month변수에 적용하고 없다면 현재 월
@@ -8,68 +10,140 @@ $date = "$year-$month-01"; // 현재 날짜
 $time = strtotime($date); // 현재 날짜의 타임스탬프
 $start_week = date('w', $time); // 1. 시작 요일
 $total_day = date('t', $time); // 2. 현재 달의 총 날짜
-$total_week = ceil(($total_day + $start_week) / 7);  // 3. 현재 달의 총 주차
+$total_week = ceil(($total_day + $start_week) / 7); // 3. 현재 달의 총 주차
+
+// 데이터를 읽어서 반을 구분하여 출력함
+$fp = fopen("save/name_table.txt", "r") or die("파일을 열 수 없습니다！");
+$rose = "";
+$sunflower = "";
+
+while (! feof($fp)) {
+    $str = fgets($fp);
+    $strarr = explode(' ', $str);
+    
+    $time = $strarr[1];
+    $clas = $strarr[2];
+    $name = $strarr[3];
+    $qr = $strarr[4];
+    
+    if (strpos($str, "2021.05") !== false) {
+        $u_arr1[] = $str;
+    }
+    for($i = 0; $i<=count($u_arr1); $i++){
+        $u_arr2[] = $u_arr1[$i];
+    }
+    if (strpos($str, "신분남") !== false && strpos($str, "2021.05") !== false) {
+        
+        $sunflower .= "<tr><td>" . $name ."</td>";
+        
+        for ($k = 1; $k <= $total_day; $k++){
+            $d= mb_strlen($k) == 2 ? $k." " : "0".$k;
+            
+            if(strpos($str, "2021.05".".".$d) !== false){
+                $sunflower .="<th>o</th>";
+            } else{
+                $sunflower .="<th>x</th>";
+            }
+        }
+        $sunflower .="</tr>";
+    }
+    
+    
+}
+fclose($fp);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>calendar</title>
-	<style type="text/css">
-		table {
-			border-spacing: 0;
-		}
-		table td {
-			text-align: center;
-		}
-	</style>
+<meta charset="UTF-8">
+<title>calendar</title>
+<style type="text/css">
+table {
+	border-spacing: 0;
+}
+
+table td {
+	text-align: center;
+}
+</style>
 </head>
+
 <body>
 	<?php echo "$year 년 $month 월" ?>
 	<!-- 현재가 1월이라 이전 달이 작년 12월인경우 -->
 	<?php if ($month == 1): ?>
 		<!-- 작년 12월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year-1 ?>&month=12">이전 달</a>
+	<a
+		href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year-1 ?>&month=12">이전
+		달</a>
 	<?php else: ?>
 		<!-- 이번 년 이전 월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year ?>&month=<?php echo $month-1 ?>">이전 달</a>
+	<a
+		href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year ?>&month=<?php echo $month-1 ?>">이전
+		달</a>
 	<?php endif ?>
 
 	<!-- 현재가 12월이라 다음 달이 내년 1월인경우 -->
 	<?php if ($month == 12): ?>
 		<!-- 내년 1월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year+1 ?>&month=1">다음 달</a>
+	<a
+		href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year+1 ?>&month=1">다음
+		달</a>
 	<?php else: ?>
 		<!-- 이번 년 다음 월 -->
-		<a href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year ?>&month=<?php echo $month+1 ?>">다음 달</a>
+	<a
+		href="https://hounjini.cafe24.com/sangwook/calendar.php/?year=<?php echo $year ?>&month=<?php echo $month+1 ?>">다음
+		달</a>
 	<?php endif ?>
 	<table border="1">
-		<tr> 
-			<th>일</th> 
-			<th>월</th> 
-			<th>화</th> 
-			<th>수</th> 
-			<th>목</th> 
-			<th>금</th> 
-			<th>토</th> 
-		</tr> 
+		<tr>
+			<th>일</th>
+			<th>월</th>
+			<th>화</th>
+			<th>수</th>
+			<th>목</th>
+			<th>금</th>
+			<th>토</th>
+		</tr>
 
 		<!-- 총 주차를 반복합니다. -->
-		<?php for ($n = 1, $i = 0; $i < $total_week; $i++): ?> 
-			<tr> 
-				<!-- 1일부터 7일 (한 주) -->
+		 <?php for ($n = 1, $i = 0; $i < $total_week; $i++): ?>
+			<tr>
+			<!-- 1일부터 7일 (한 주) -->
 				<?php for ($k = 0; $k < 7; $k++): ?> 
-					<td> 
-						<!-- 시작 요일부터 마지막 날짜까지만 날짜를 보여주도록 -->
+					<td>
+				<!-- 시작 요일부터 마지막 날짜까지만 날짜를 보여주도록 -->
 						<?php if ( ($n > 1 || $k >= $start_week) && ($total_day >= $n) ): ?>
-							<!-- 현재 날짜를 보여주고 1씩 더해줌 -->
-							<a href="https://hounjini.cafe24.com/sangwook/read.php/?year=<?php echo $year ?>&month=<?php echo $month ?>&date=<?php echo $n ?>"><?php echo $n++ ?> </a>
+							<!-- 현재 날짜를 보여주고 1씩 더해줌 --> <a
+				href="https://hounjini.cafe24.com/sangwook/read.php/?year=<?php echo $year ?>&month=<?php echo $month ?>&day=<?php echo $n ?>"><?php echo $n++ ?> </a>
 						<?php endif ?>
 					</td> 
 				<?php endfor; ?> 
 			</tr> 
 		<?php endfor; ?> 
+	</table>
+	<br>
+	<br>
+	<br>
+	<table border="1">
+		<tr>
+			<th>공백</th>
+			<?php for ($k = 1; $k <= $total_day; $k++): ?> 
+			<th>
+				<?php echo $k ?>
+			</th> 
+			<?php endfor; ?>
+		</tr>
+		<tr>
+			<th>공백</th><th colspan="<?php print $total_day?>">해바라기</th>
+		</tr>
+		
+			<?php print $sunflower?> 
+		
+		<tr>
+			<th>공백</th><th colspan="<?php print $total_day?>">장미</th>
+		</tr>
 	</table>
 </body>
 </html>
